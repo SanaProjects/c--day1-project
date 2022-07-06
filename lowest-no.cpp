603ff62bde813f3f1f2e47a660361495ba364bd7
SanaProjects <sanaswastika21@gmail.com>
2022-07-06 17:54:47 +0530

    lowest number

---
+++ lowest-no.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cout<<endl<<"Which one is the lowest no. ?";
+    cout<<endl<<"Put the number 1 = ";
+    cin>>a;
+
+    cout<<endl<<"Put the number 2 = ";
+    cin>>b;
+
+    
+
+    
+    if(a<b)
+    {
+        cout<<endl<<"Number 1 is lowest.";
+    }
+    else
+    {
+        cout<<endl<<"Number 2 is lowest.";   
+    }
+    
+
+    return 0;
+}
